3027492f2610bdad110e8f6422264c8840b4ee37
csh2638 <tmdtmdtmd4@gmail.com>
2023-04-04 23:27:08 +0900

    캐릭터 에셋 및 이동 에니메이션 추가 적용

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,75 +7,117 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     float _speed = 10.0f;
 
-    bool _moveToDest = false;
     Vector3 _destPos;
 
+
+    public enum PlayerState
+    {
+        Die,
+        Moving,
+        Idle,
+
+    }
+
+    PlayerState _state = PlayerState.Idle;
 
     // Start is called before the first frame update
     void Start()
     {
-        Managers.Input.KeyAction -= OnKeyboard;
-        Managers.Input.KeyAction += OnKeyboard;
+        //Managers.Input.KeyAction -= OnKeyboard;
+        //Managers.Input.KeyAction += OnKeyboard;
 
         Managers.Input.MouseAction -= OnMouseClicked;
-        Managers.Input.MouseAction += OnMouseClicked;
+        Managers.Input.MouseAction += OnMouseClicked;        
     }
 
+    void UpdateMoving()
+    {
+        Vector3 dir = _destPos - transform.position;
+        if (dir.magnitude < 0.01f)
+        {
+            _state = PlayerState.Idle;
+        }
+        else
+        {
+            float moveDist = Mathf.Clamp(_speed * Time.deltaTime, 0, dir.magnitude);
+
+            transform.position = transform.position + dir.normalized * moveDist;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 40 * Time.deltaTime);
+        }
+
+        wait_run_ratio = Mathf.Lerp(wait_run_ratio, 0, 20.0f * Time.deltaTime);
+        Animator anim = GetComponent<Animator>();
+        anim.SetFloat("wait_run_ratio", wait_run_ratio);
+        anim.Play("WAIT_RUN");
+    }
+    void UpdateIdle()
+    {
+        wait_run_ratio = Mathf.Lerp(wait_run_ratio, 1, 10.0f * Time.deltaTime);
+        Animator anim = GetComponent<Animator>();
+        anim.SetFloat("wait_run_ratio", wait_run_ratio);
+        anim.Play("WAIT_RUN");
+    }
+    void UpdateDie()
+    {
+
+    }
+
+
+
+
+    float wait_run_ratio = 0;
     // Update is called once per frame
     void Update()
     {
-        if(_moveToDest)
+        switch (_state)
         {
-            Vector3 dir = _destPos - transform.position;
-            if(dir.magnitude < 0.1f)
-            {
-                _moveToDest = false;
-            }
-            else
-            {
-                float moveDist = Mathf.Clamp(_speed * Time.deltaTime, 0, dir.magnitude);
+            case PlayerState.Moving:
+                UpdateMoving();
+                break;
+            case PlayerState.Idle:
+                UpdateIdle();
+                break;
+            case PlayerState.Die:
+                UpdateDie();
+                break;
 
-                transform.position = transform.position + dir.normalized * moveDist;
-                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 40 * Time.deltaTime);
-            }
         }
-        
     }
 
-    void OnKeyboard()
-    {
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.2f);
-            transform.position += Vector3.forward * Time.deltaTime * _speed;
-        }
+    //void OnKeyboard()
+    //{
+    //    if (Input.GetKey(KeyCode.W))
+    //    {
+    //        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.2f);
+    //        transform.position += Vector3.forward * Time.deltaTime * _speed;
+    //    }
 
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.2f);
-            transform.position += Vector3.back * Time.deltaTime * _speed;
-        }
+    //    if (Input.GetKey(KeyCode.S))
+    //    {
+    //        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.2f);
+    //        transform.position += Vector3.back * Time.deltaTime * _speed;
+    //    }
 
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.2f);
-            transform.position += Vector3.left * Time.deltaTime * _speed;
-        }
+    //    if (Input.GetKey(KeyCode.A))
+    //    {
+    //        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.2f);
+    //        transform.position += Vector3.left * Time.deltaTime * _speed;
+    //    }
 
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
-            transform.position += Vector3.right * Time.deltaTime * _speed;
-        }
+    //    if (Input.GetKey(KeyCode.D))
+    //    {
+    //        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
+    //        transform.position += Vector3.right * Time.deltaTime * _speed;
+    //    }
 
-        _moveToDest = false;
-    }
+    //    _moveToDest = false;
+    //}
 
 
 
     void OnMouseClicked(Settings.MouseEvent evt)
     {
-        if(evt != Settings.MouseEvent.Click)
+        if(_state == PlayerState.Die)
         {
             return;
         }
@@ -95,8 +137,11 @@ public class PlayerController : MonoBehaviour
         if (Physics.Raycast(ray, out hit, 100.0f, mask))
         {
             _destPos = hit.point;
+            //_destPos.x = hit.point.x;
+            //_destPos.z = hit.point.z;
+            //_destPos.y = 0;
             Debug.Log(_destPos);
-            _moveToDest = true;
+            _state = PlayerState.Moving;
             //Debug.Log($"Raycast hit{hit.collider.gameObject.name}");
 
         }
